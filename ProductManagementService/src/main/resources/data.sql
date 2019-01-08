be97e588cd6cd75cf1b928acc2798a79f096f5cb
ayushig20 <ashish1988anshul@gmail.com>
2019-01-08 13:11:31 +0530

    added certificate and new product list

--- ProductManagementService/src/main/resources/data.sql
+++ ProductManagementService/src/main/resources/data.sql
@@ -1,8 +1,125 @@
 insert into product
-values(101,'ashish', 'Type1');
+values(101,'Pears', 'SOAP');
 
 insert into product
-values(102,'Mani', 'Type2');
+values(102,'Rin', 'SOAP');
 
 insert into product
-values(103,'John', 'Type3');
+values(103,'LifeBoy', 'SOAP');
+
+insert into product
+values(104,'Dettol', 'SOAP');
+
+insert into product
+values(105,'Vim', 'SOAP');
+
+insert into product
+values(106,'Dove', 'SOAP');
+
+insert into product
+values(107,'Hamam', 'SOAP');
+
+insert into product
+values(108,'Godrej', 'SOAP');
+
+insert into product
+values(109,'Nivea', 'SOAP');
+
+insert into product
+values(110,'Liril', 'SOAP');
+
+insert into product
+values(111,'Mysore Sandal', 'SOAP');
+
+insert into product
+values(112,'Fiama Di Wills', 'SOAP');
+
+insert into product
+values(113,'Khadi Sandalwood Mud Scrub Bar', 'SOAP');
+
+insert into product
+values(114,'Himalaya Herbals Refreshing Cucumber & Coconut Soap', 'SOAP');
+
+insert into product
+values(115,'Nirma', 'SOAP');
+
+insert into product
+values(116,'Clinic Plus', 'SHAMPOO');
+
+insert into product
+values(117,'Lux', 'SHAMPOO');
+
+insert into product
+values(118,'Sunslik', 'SHAMPOO');
+
+insert into product
+values(119,'Head & Shoulders', 'SHAMPOO');
+
+insert into product
+values(120,'Scalp Plus', 'SHAMPOO');
+
+insert into product
+values(121,'Indulekha', 'SHAMPOO');
+
+insert into product
+values(122,'Pantene', 'SHAMPOO');
+
+insert into product
+values(123,'Garnier Fructis', 'SHAMPOO');
+
+insert into product
+values(124,'Matrix Biolage Ultra Hydrasource Shampoo', 'SHAMPOO');
+
+insert into product
+values(125,'Dove Intense Repair Damage Therapy Shampoo', 'SHAMPOO');
+
+insert into product
+values(126,'Garnier Ultra Blends Mythic Olive Shampoo', 'SHAMPOO');
+
+insert into product
+values(127,'Yves Rocher Lissage Smoothing Shampoo', 'SHAMPOO');
+
+insert into product
+values(128,'Fabindia Silk Protein Shampoo', 'SHAMPOO');
+
+insert into product
+values(129,'Blossom Kochhar AromaMagic Aloevera Cream Shampooo', 'SHAMPOO');
+
+insert into product
+values(130,'Biotique Bio Soya Protein Fresh Nourishing Shampoo', 'SHAMPOO');
+
+insert into product
+values(131,'L’Oreal Paris Smooth Intense Shampoo', 'SHAMPOO');
+
+insert into product
+values(132,'Organix Hydrating Macadamia Oil Shampoo', 'SHAMPOO');
+
+insert into product
+values(133,'Wheel', 'DETERGENT POWDER');
+
+insert into product
+values(134,'Surf Excel Easy Wash', 'DETERGENT POWDER');
+
+insert into product
+values(135,'Surf Excel Matic', 'DETERGENT POWDER');
+
+insert into product
+values(136,'Tide Plus', 'DETERGENT POWDER');
+
+insert into product
+values(137,'Ariel Matic', 'DETERGENT POWDER');
+
+insert into product
+values(138,'Patanjali', 'DETERGENT POWDER');
+
+insert into product
+values(139,'Express powder', 'DETERGENT POWDER');
+
+insert into product
+values(140,'Ghadi', 'DETERGENT POWDER');
+
+insert into product
+values(141,'Safed', 'DETERGENT POWDER');
+
+insert into product
+values(142,'Rin powder', 'DETERGENT POWDER');
