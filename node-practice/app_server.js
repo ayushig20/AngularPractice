3cc6cc76b25da8b9167f9c567ab92da20cd2cd76
ayushig20 <ashish1988anshul@gmail.com>
2019-02-25 16:54:53 +0530

    added node practice code

--- node-practice/app_server.js
+++ node-practice/app_server.js
@@ -1,12 +1,27 @@
 var http = require('http');
 var fs = require('fs');
 
-var myReadStream = fs.createReadStream(__dirname + '/readMe.txt','utf8');
+//var myReadStream = fs.createReadStream(__dirname + '/readMe.txt','utf8');
 
 var server = http.createServer(function (req, res) {
     console.log('Request : ' + req.url);
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    myReadStream.pipe(res);
+    if(req.url === '/home' || req.url === '/'){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        fs.createReadStream(__dirname+'/index.html').pipe(res);
+    }
+    else if(req.url === '/contact'){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        fs.createReadStream(__dirname+'/contact.html').pipe(res);
+    }
+    else if(req.url === '/api/ninjas'){
+        var ninjas = [{name:'ashish',age:30},{name:'ayushi',age:27},{name:'chikku',age: 9}];
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify(ninjas));
+    }else{
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        fs.createReadStream(__dirname+'/404.html').pipe(res);
+    }
+     
     //res.end('heyyyyyyyyyy from server...')
 });
 
