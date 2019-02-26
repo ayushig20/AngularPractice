7c0b3490f8ad2740a76787b44f41d0c92d51e8d7
ayushig20 <ashish1988anshul@gmail.com>
2019-02-26 22:10:24 +0530

    added node practice code

--- node-practice/app_express.js
+++ node-practice/app_express.js
@@ -1,16 +1,22 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine','ejs');
+
 app.get('/',function(req,res){
-    res.sendFile(__dirname+'/index.html');
+    //res.sendFile(__dirname+'/index.html');
+    res.render('index');
 });
 
 app.get('/contact',function(req,res){
-    res.sendFile(__dirname+'/contact.html');
+    //res.sendFile(__dirname+'/contact.html');
+    res.render('contact');
 });
 
 app.get('/profile/:name',function(req,res){
-    res.send('You are viewing the profile of '+ req.params.name);
+    //res.send('You are viewing the profile of '+ req.params.name);
+    var data = {age:27,job:'IT services',hobbies:['playing','cooking','eating']}
+    res.render('profile',{person: req.params.name, data:data})
 });
 
 app.listen(3000);
